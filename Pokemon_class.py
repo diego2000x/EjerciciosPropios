56b470aebd19a49e4979bf6d114643ae15412906
Diego Ramirez <diegoramirez2004@gmail.com>
2025-06-05 20:25:39 -0400

    Update Pokemon_class.py

--- Pokemon_class.py
+++ Pokemon_class.py
@@ -12,6 +12,21 @@ class Pokemon:
     
     def atacar(self):
         print(f"{self.nombre} ha realizado un ataque tipo {self.tipo}!")
+    
+    def subirNivel(self):
+        self.nivel += 1
+        print(f"{self.nombre} ha subido al nivel {self.nivel}")
+
+    def batalla(self, enemigo):
+        if self.nivel > enemigo.nivel:
+            self.atacar()
+            enemigo.atacar()
+            print(f"{self.nombre} ha derrotado a {enemigo.nombre}!")
+            self.subirNivel()
+        elif self.nivel < enemigo.nivel:
+            print(f"{self.nombre} ha sido derrotado por {enemigo.nombre}.")
+        else:
+            print("Es un empate!")
 
 SaludarPokemon("Pikachu")
 SaludarPokemon("Charmander")
@@ -22,9 +37,7 @@ charmander = Pokemon("Charmander", "fuego", 9)
 print(pikachu.info())
 print(charmander.info())
 
-pikachu.atacar()
-charmander.atacar()
-pikachu.atacar()
+Pokemon.batalla(pikachu, charmander)
 
-print(f"{charmander.nombre} ha sido derrotado, {pikachu.nombre} ganó el combate!")
+
 
