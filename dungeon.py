edabd41a955a89080345134d803dbef5241fa79e
Diego Ramirez <diegoramirez2004@gmail.com>
2025-06-10 03:54:06 -0400

    Update dungeon.py

--- dungeon.py
+++ dungeon.py
@@ -1,7 +1,11 @@
 import os
+import msvcrt #Importa opciones de Windows como presionar cualquier tecla para continuar.
 
 def limpiarPantalla():
     os.system('cls')
+
+def pressAnyKey():
+    msvcrt.getch()
 
 limpiarPantalla()
 
@@ -16,11 +20,46 @@ class entidad:
      def info(self):
         print(f"{self.nombre} - LVL {self.nivel}")
         print(f"{self.hp} PS")
+     
+     def aparecio(self):
+         print(f"Un {self.nombre} ha aparecido!")
+     
+     def atacar(self, enemigo):
+         enemigo.hp -= self.ataque
+         print(f"{self.nombre} atacó a {enemigo.nombre} quitandole {self.ataque} PS.")
+
+     def batalla(self, enemigo):
+         enemigo.aparecio()
+         pressAnyKey()
+         pelearOHuir = input("Quieres pelear con el? s/n: ").lower()
+         if pelearOHuir == "s":
+             limpiarPantalla()
+             self.info()
+             enemigo.info()
+             pressAnyKey()
+             limpiarPantalla()
+             self.atacar(enemigo)
+             pressAnyKey()
+             enemigo.atacar(self)
+             pressAnyKey()
+             self.info()
+             enemigo.info()
+
+             if self.hp > 0 or enemigo.hp > 0:
+                 pass
+
+         else:
+             print("Has huido.")
+
 
 guerrero = entidad("Guerrero", 20, 5, 1)
 slime = entidad("Slime", 10, 1, 1)
 pajaro = entidad("Pajarraco", 12, 2, 1)
 goblin = entidad("Goblin", 14, 4, 1)
+orco = entidad("Orco", 20, 5, 1)
 
-print(f"Bienvenido al calabozo {guerrero.nombre}")
+print(f"Bienvenido al calabozo {guerrero.nombre}!")
+pressAnyKey()
+
+guerrero.batalla(slime)
 
