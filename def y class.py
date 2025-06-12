e6bbf266cd5483dbf2f25530ac9475c18c983f61
Diego Ramirez <diegoramirez2004@gmail.com>
2025-06-12 04:47:50 -0400

    Update def y class.py

--- def y class.py
+++ def y class.py
@@ -8,11 +8,18 @@ class Persona:
         self.nombre = nombre
         self.edad = edad
         self.ciudad = ciudad
+        
     def info(self):
-        return f"Su nombre es {self.nombre}, tiene {self.edad} años y es de {self.ciudad}."
+        return f"Su nombre es {self.nombre}, tiene {self.edad} años y es de {self.ciudad}. "
+    
+    def gerundio(self):
+        if self.ciudad == "Colin":
+            return f"{self.nombre} es Colinense"
+        if self.ciudad == "Talca":
+            return f"{self.nombre} es Talquino"
     
 diego = Persona("Diego", 30, "Talca")
 javi = Persona("Javiera", 25, "Colin") 
 
-print(diego.info())
-print(javi.info())
+print(diego.info()+ diego.gerundio())
+print(javi.info()+ javi.gerundio())
